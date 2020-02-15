cd6a057b5f988f645961f35390eee97678b10b4a
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2020-02-15 15:00:52 +0200

    Refactor CurrentWeather positioning

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -15,22 +15,22 @@ const CurrentWeather = ({ name, currentWeather }: Props) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.topLeft}>
-        <div className={styles.name}>{name}</div>
-        <div className={styles.smallText}>{description}</div>
+      <div className={styles.topRow}>
+        <div className={styles.topLeft}>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.smallText}>{description}</div>
+        </div>
+        <div className={styles.topRight}>
+          <div>{icon}</div>
+          <div className={styles.temp}>{temp} °C</div>
+        </div>
       </div>
-      <div className={styles.topRight}>
-        <div>{icon}</div>
-        <div className={styles.temp}>{temp} °C</div>
-      </div>
-      <div className={styles.bottomLeft}>
-        <div>
+      <div className={styles.bottomRow}>
+        <div className={styles.bottomLeft}>
           <div className={styles.date}>{monthAndDay}</div>
           <div className={styles.smallText}>{time}</div>
         </div>
-      </div>
-      <div className={styles.bottomRight}>
-        <div>
+        <div className={styles.bottomRight}>
           <div className={styles.smallText}>Wind: {wind} m/s</div>
           <div className={styles.smallText}>Humidity: {humidity} %</div>
           <div className={styles.smallText}>Precipitation (3h): {rain} mm</div>
