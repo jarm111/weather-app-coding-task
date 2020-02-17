5eb9e8a1b7c5d46382b20d1b28d7fa0ce3f269cd
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2020-02-17 14:27:54 +0200

    Updates

---
+++ precache-manifest.073df4f0268a6b7e6ca151192a131b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa1238e26e06f9c2e491ba90ca1fefe",
+    "url": "/weather-app-coding-task/index.html"
+  },
+  {
+    "revision": "f3278ed2cf1ec59a6bee",
+    "url": "/weather-app-coding-task/static/css/main.d0495976.chunk.css"
+  },
+  {
+    "revision": "a728472ab2714ca9b740",
+    "url": "/weather-app-coding-task/static/js/2.fdecc3ee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather-app-coding-task/static/js/2.fdecc3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3278ed2cf1ec59a6bee",
+    "url": "/weather-app-coding-task/static/js/main.1dc86fa0.chunk.js"
+  },
+  {
+    "revision": "fbdf46393b7b8a38ac90",
+    "url": "/weather-app-coding-task/static/js/runtime-main.fe695fa3.js"
+  }
+]);
